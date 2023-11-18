5765dc152c5c749d454e7098e2f217a7a2a3475e
Manel Soler <manelsoler@gmail.com>
2023-11-18 14:29:20 +0100

    fetch data first endopoint

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
-import { App } from './src/App'
+import { App } from './src/App.jsx'
 
 const root = createRoot(document.getElementById('app'))
 
-root.render(<h1>Hellow!</h1>)
+root.render(<App />)
--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import { useState, useEffect } from 'react'
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstword}?fontSize=50&fontColor=red&json=true`
+
 export function App () {
-  return <h1>Cats App</h1>
+  const [fact, setFact] = useState()
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const { fact } = data
+        setFact(fact)
+
+        const firstWord = fact.split(' ')[0]
+        console.log(firstWord)
+      })
+  }, [])
+
+  return (
+    <main>
+      <h1>Vite Cats App</h1>
+      {fact && <p>{fact}</p>}
+    </main>
+  )
 }
